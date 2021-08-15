8cedad204a6ea1b14da49cbd85a18fa5548e7f31
Shreyas Patankar <ssp1996@gmail.com>
2021-08-15 13:12:54 +0530

    HTTPS support

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,16 @@
 require("dotenv").config();
 import express, { Application, Request, Response } from "express";
+import fs from "fs";
+import https from "https";
 
 const app: Application = express();
+
+const key = fs.readFileSync(__dirname + "/../../certs/selfsigned.key");
+const cert = fs.readFileSync(__dirname + "/../../certs/selfsigned.crt");
+const options = {
+  key: key,
+  cert: cert,
+};
 
 const port: number = parseInt(process.env.PORT || "8080");
 
@@ -9,6 +18,8 @@ app.get("/toto", (req: Request, res: Response) => {
   res.send("Hello toto");
 });
 
-app.listen(port, function () {
-  console.log(`App is listening on port ${port} !`);
+const server = https.createServer(options, app);
+
+server.listen(port, () => {
+  console.log("server starting on port : " + port);
 });
