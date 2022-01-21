bd30b497a4f75c9cbf0f26c1f257009b97db4da7
Shreyas Patankar <ssp1996@gmail.com>
2022-01-21 09:52:58 +0530

    Update scripts

--- src/constants/scripts.ts
+++ src/constants/scripts.ts
@@ -2,707 +2,806 @@ export const SCRIPTS: Record<string, number> = {};
 
 const raw = [
   {
-    instrument_token: "12477442",
-    name: "NIFTY",
-  },
-  {
-    instrument_token: "12476930",
-    name: "BANKNIFTY",
-  },
-  {
-    instrument_token: "12477698",
+    instrument_token: 13278978,
     name: "AARTIIND",
   },
   {
-    instrument_token: "12479490",
+    instrument_token: 13279746,
+    name: "ABBOTINDIA",
+  },
+  {
+    instrument_token: 14828290,
+    name: "ABCAPITAL",
+  },
+  {
+    instrument_token: 13280002,
     name: "ABFRL",
   },
   {
-    instrument_token: "12479746",
+    instrument_token: 13281794,
     name: "ACC",
   },
   {
-    instrument_token: "12480514",
+    instrument_token: 13282050,
     name: "ADANIENT",
   },
   {
-    instrument_token: "12485634",
+    instrument_token: 13282562,
     name: "ADANIPORTS",
   },
   {
-    instrument_token: "12485890",
+    instrument_token: 13282818,
     name: "ALKEM",
   },
   {
-    instrument_token: "12487682",
+    instrument_token: 13283074,
     name: "AMARAJABAT",
   },
   {
-    instrument_token: "12487938",
+    instrument_token: 13283330,
     name: "AMBUJACEM",
   },
   {
-    instrument_token: "12488450",
+    instrument_token: 13283586,
     name: "APLLTD",
   },
   {
-    instrument_token: "12488706",
+    instrument_token: 13283842,
     name: "APOLLOHOSP",
   },
   {
-    instrument_token: "12491010",
+    instrument_token: 13284098,
     name: "APOLLOTYRE",
   },
   {
-    instrument_token: "12491266",
+    instrument_token: 13284354,
     name: "ASHOKLEY",
   },
   {
-    instrument_token: "12492546",
+    instrument_token: 13284610,
     name: "ASIANPAINT",
   },
   {
-    instrument_token: "13213698",
+    instrument_token: 13284866,
     name: "ASTRAL",
   },
   {
-    instrument_token: "12492802",
+    instrument_token: 13285890,
+    name: "ATUL",
+  },
+  {
+    instrument_token: 13286146,
     name: "AUBANK",
   },
   {
-    instrument_token: "12493058",
+    instrument_token: 13286402,
     name: "AUROPHARMA",
   },
   {
-    instrument_token: "12493314",
+    instrument_token: 13286658,
     name: "AXISBANK",
   },
   {
-    instrument_token: "12496130",
+    instrument_token: 13286914,
     name: "BAJAJ-AUTO",
   },
   {
-    instrument_token: "12496386",
+    instrument_token: 13287170,
     name: "BAJAJFINSV",
   },
   {
-    instrument_token: "12496642",
+    instrument_token: 13287938,
     name: "BAJFINANCE",
   },
   {
-    instrument_token: "12496898",
+    instrument_token: 13288450,
     name: "BALKRISIND",
   },
   {
-    instrument_token: "12497154",
+    instrument_token: 14834434,
+    name: "BALRAMCHIN",
+  },
+  {
+    instrument_token: 13288706,
     name: "BANDHANBNK",
   },
   {
-    instrument_token: "12497410",
+    instrument_token: 13288962,
     name: "BANKBARODA",
   },
   {
-    instrument_token: "12499202",
+    instrument_token: 13278210,
+    name: "BANKNIFTY",
+  },
+  {
+    instrument_token: 13289218,
     name: "BATAINDIA",
   },
   {
-    instrument_token: "12499458",
+    instrument_token: 13289474,
     name: "BEL",
   },
   {
-    instrument_token: "12501250",
+    instrument_token: 13289730,
     name: "BERGEPAINT",
   },
   {
-    instrument_token: "12501506",
+    instrument_token: 13289986,
     name: "BHARATFORG",
   },
   {
-    instrument_token: "12503298",
+    instrument_token: 13290242,
     name: "BHARTIARTL",
   },
   {
-    instrument_token: "12503554",
+    instrument_token: 13291010,
     name: "BHEL",
   },
   {
-    instrument_token: "12506370",
+    instrument_token: 13291266,
     name: "BIOCON",
   },
   {
-    instrument_token: "12506626",
+    instrument_token: 13291522,
     name: "BOSCHLTD",
   },
   {
-    instrument_token: "12507394",
+    instrument_token: 13291778,
     name: "BPCL",
   },
   {
-    instrument_token: "12507650",
+    instrument_token: 13292034,
     name: "BRITANNIA",
   },
   {
-    instrument_token: "12507906",
+    instrument_token: 13292290,
+    name: "BSOFT",
+  },
+  {
+    instrument_token: 13292546,
     name: "CADILAHC",
   },
   {
-    instrument_token: "12508162",
+    instrument_token: 13292802,
     name: "CANBK",
   },
   {
-    instrument_token: "13730050",
+    instrument_token: 13293058,
     name: "CANFINHOME",
   },
   {
-    instrument_token: "12508418",
+    instrument_token: 13293314,
+    name: "CHAMBLFERT",
+  },
+  {
+    instrument_token: 13293570,
     name: "CHOLAFIN",
   },
   {
-    instrument_token: "12508674",
+    instrument_token: 13293826,
     name: "CIPLA",
   },
   {
-    instrument_token: "12508930",
+    instrument_token: 13294082,
     name: "COALINDIA",
   },
   {
-    instrument_token: "12509186",
+    instrument_token: 13294338,
     name: "COFORGE",
   },
   {
-    instrument_token: "12509442",
+    instrument_token: 13294594,
     name: "COLPAL",
   },
   {
-    instrument_token: "12509698",
+    instrument_token: 13294850,
     name: "CONCOR",
   },
   {
-    instrument_token: "12516098",
+    instrument_token: 13295106,
     name: "COROMANDEL",
   },
   {
-    instrument_token: "12516354",
+    instrument_token: 13295874,
+    name: "CROMPTON",
+  },
+  {
+    instrument_token: 13296130,
     name: "CUB",
   },
   {
-    instrument_token: "12516610",
+    instrument_token: 13296386,
     name: "CUMMINSIND",
   },
   {
-    instrument_token: "12516866",
+    instrument_token: 13296642,
     name: "DABUR",
   },
   {
-    instrument_token: "12518658",
+    instrument_token: 13296898,
+    name: "DALBHARAT",
+  },
+  {
+    instrument_token: 13297154,
     name: "DEEPAKNTR",
   },
   {
-    instrument_token: "12518914",
+    instrument_token: 13297410,
+    name: "DELTACORP",
+  },
+  {
+    instrument_token: 13297666,
     name: "DIVISLAB",
   },
   {
-    instrument_token: "13733890",
+    instrument_token: 13297922,
     name: "DIXON",
   },
   {
-    instrument_token: "12521474",
+    instrument_token: 13298178,
     name: "DLF",
   },
   {
-    instrument_token: "12521730",
+    instrument_token: 13298434,
     name: "DRREDDY",
   },
   {
-    instrument_token: "12525314",
+    instrument_token: 13298690,
     name: "EICHERMOT",
   },
   {
-    instrument_token: "12525570",
+    instrument_token: 13298946,
     name: "ESCORTS",
   },
   {
-    instrument_token: "12525826",
+    instrument_token: 13299202,
     name: "EXIDEIND",
   },
   {
-    instrument_token: "12526082",
+    instrument_token: 13299458,
     name: "FEDERALBNK",
   },
   {
-    instrument_token: "12477186",
-    name: "FINNIFTY",
+    instrument_token: 13299714,
+    name: "FSL",
   },
   {
-    instrument_token: "12528386",
+    instrument_token: 13299970,
     name: "GAIL",
   },
   {
-    instrument_token: "12528642",
+    instrument_token: 13300226,
     name: "GLENMARK",
   },
   {
-    instrument_token: "12530178",
+    instrument_token: 13300482,
     name: "GMRINFRA",
   },
   {
-    instrument_token: "12530434",
+    instrument_token: 14847490,
+    name: "GNFC",
+  },
+  {
+    instrument_token: 13300738,
     name: "GODREJCP",
   },
   {
-    instrument_token: "12530690",
+    instrument_token: 13300994,
     name: "GODREJPROP",
   },
   {
-    instrument_token: "12530946",
+    instrument_token: 13301250,
     name: "GRANULES",
   },
   {
-    instrument_token: "12531202",
+    instrument_token: 13301506,
     name: "GRASIM",
   },
   {
-    instrument_token: "12531458",
+    instrument_token: 13301762,
+    name: "GSPL",
+  },
+  {
+    instrument_token: 13302018,
     name: "GUJGASLTD",
   },
   {
-    instrument_token: "13738242",
+    instrument_token: 13302274,
     name: "HAL",
   },
   {
-    instrument_token: "12531714",
+    instrument_token: 13302530,
     name: "HAVELLS",
   },
   {
-    instrument_token: "12531970",
+    instrument_token: 13302786,
     name: "HCLTECH",
   },
   {
-    instrument_token: "12532226",
+    instrument_token: 13303042,
     name: "HDFC",
   },
   {
-    instrument_token: "12532482",
+    instrument_token: 13303298,
     name: "HDFCAMC",
   },
   {
-    instrument_token: "12532738",
+    instrument_token: 13303554,
     name: "HDFCBANK",
   },
   {
-    instrument_token: "12532994",
+    instrument_token: 13303810,
     name: "HDFCLIFE",
   },
   {
-    instrument_token: "12533250",
+    instrument_token: 13304066,
     name: "HEROMOTOCO",
   },
   {
-    instrument_token: "12533506",
+    instrument_token: 13304322,
     name: "HINDALCO",
   },
   {
-    instrument_token: "12533762",
+    instrument_token: 14853122,
+    name: "HINDCOPPER",
+  },
+  {
+    instrument_token: 13304578,
     name: "HINDPETRO",
   },
   {
-    instrument_token: "12534530",
+    instrument_token: 13304834,
     name: "HINDUNILVR",
   },
   {
-    instrument_token: "12534786",
+    instrument_token: 14854402,
+    name: "HONAUT",
+  },
+  {
+    instrument_token: 13305090,
     name: "IBULHSGFIN",
   },
   {
-    instrument_token: "12535042",
+    instrument_token: 13305346,
     name: "ICICIBANK",
   },
   {
-    instrument_token: "12535298",
+    instrument_token: 13305602,
     name: "ICICIGI",
   },
   {
-    instrument_token: "12535554",
+    instrument_token: 13305858,
     name: "ICICIPRULI",
   },
   {
-    instrument_token: "12535810",
+    instrument_token: 13306114,
     name: "IDEA",
   },
   {
-    instrument_token: "12536066",
+    instrument_token: 14859010,
+    name: "IDFC",
+  },
+  {
+    instrument_token: 13306370,
     name: "IDFCFIRSTB",
   },
   {
-    instrument_token: "13743106",
+    instrument_token: 13306626,
     name: "IEX",
   },
   {
-    instrument_token: "12536322",
+    instrument_token: 13306882,
     name: "IGL",
   },
   {
-    instrument_token: "12537090",
+    instrument_token: 13307138,
     name: "INDHOTEL",
   },
   {
-    instrument_token: "13744386",
+    instrument_token: 13307394,
+    name: "INDIACEM",
+  },
+  {
+    instrument_token: 13307650,
     name: "INDIAMART",
   },
   {
-    instrument_token: "12537346",
+    instrument_token: 13307906,
     name: "INDIGO",
   },
   {
-    instrument_token: "12537602",
+    instrument_token: 13308162,
     name: "INDUSINDBK",
   },
   {
-    instrument_token: "12537858",
+    instrument_token: 2,
+    name: "INDUSINDBK",
+  },
+  {
+    instrument_token: 13308418,
     name: "INDUSTOWER",
   },
   {
-    instrument_token: "12538114",
+    instrument_token: 13308674,
     name: "INFY",
   },
   {
-    instrument_token: "12538370",
+    instrument_token: 13308930,
     name: "IOC",
   },
   {
-    instrument_token: "13746434",
+    instrument_token: 13309186,
     name: "IPCALAB",
   },
   {
-    instrument_token: "12538626",
+    instrument_token: 13309442,
     name: "IRCTC",
   },
   {
-    instrument_token: "12538882",
+    instrument_token: 13310722,
     name: "ITC",
   },
   {
-    instrument_token: "12539650",
+    instrument_token: 13310978,
     name: "JINDALSTEL",
   },
   {
-    instrument_token: "12539906",
+    instrument_token: 13312258,
+    name: "JKCEMENT",
+  },
+  {
+    instrument_token: 13312514,
     name: "JSWSTEEL",
   },
   {
-    instrument_token: "12540162",
+    instrument_token: 13312770,
     name: "JUBLFOOD",
   },
   {
-    instrument_token: "12540418",
+    instrument_token: 13313026,
     name: "KOTAKBANK",
   },
   {
-    instrument_token: "12540674",
+    instrument_token: 13313282,
     name: "L&TFH",
   },
   {
-    instrument_token: "12540930",
+    instrument_token: 13313538,
     name: "LALPATHLAB",
   },
   {
-    instrument_token: "12541186",
+    instrument_token: 13313794,
+    name: "LAURUSLABS",
+  },
+  {
+    instrument_token: 13314050,
     name: "LICHSGFIN",
   },
   {
-    instrument_token: "12541442",
+    instrument_token: 13314306,
     name: "LT",
   },
   {
-    instrument_token: "12541698",
+    instrument_token: 13314562,
     name: "LTI",
   },
   {
-    instrument_token: "12541954",
+    instrument_token: 13314818,
     name: "LTTS",
   },
   {
-    instrument_token: "12542210",
+    instrument_token: 13315074,
     name: "LUPIN",
   },
   {
-    instrument_token: "12542466",
+    instrument_token: 13315330,
     name: "M&M",
   },
   {
-    instrument_token: "12542722",
+    instrument_token: 13315586,
     name: "M&MFIN",
   },
   {
-    instrument_token: "12542978",
+    instrument_token: 13315842,
     name: "MANAPPURAM",
   },
   {
-    instrument_token: "12543234",
+    instrument_token: 13316098,
     name: "MARICO",
   },
   {
-    instrument_token: "12543490",
+    instrument_token: 13316354,
     name: "MARUTI",
   },
   {
-    instrument_token: "12543746",
+    instrument_token: 13317122,
     name: "MCDOWELL-N",
   },
   {
-    instrument_token: "13753602",
+    instrument_token: 13317378,
     name: "MCX",
   },
   {
-    instrument_token: "12544514",
+    instrument_token: 13317634,
     name: "METROPOLIS",
   },
   {
-    instrument_token: "12544770",
+    instrument_token: 13317890,
     name: "MFSL",
   },
   {
-    instrument_token: "12545026",
+    instrument_token: 13318146,
     name: "MGL",
   },
   {
-    instrument_token: "12545282",
+    instrument_token: 13318402,
     name: "MINDTREE",
   },
   {
-    instrument_token: "12545538",
+    instrument_token: 13318658,
     name: "MOTHERSUMI",
   },
   {
-    instrument_token: "12545794",
+    instrument_token: 13319426,
     name: "MPHASIS",
   },
   {
-    instrument_token: "12546050",
+    instrument_token: 13319682,
     name: "MRF",
   },
   {
-    instrument_token: "12546306",
+    instrument_token: 13319938,
     name: "MUTHOOTFIN",
   },
   {
-    instrument_token: "12546562",
+    instrument_token: 13320194,
     name: "NAM-INDIA",
   },
   {
-    instrument_token: "12551426",
+    instrument_token: 13320450,
     name: "NATIONALUM",
   },
   {
-    instrument_token: "12551682",
+    instrument_token: 13320706,
     name: "NAUKRI",
   },
   {
-    instrument_token: "12552962",
+    instrument_token: 13320962,
     name: "NAVINFLUOR",
   },
   {
-    instrument_token: "12553218",
+    instrument_token: 14872578,
+    name: "NBCC",
+  },
+  {
+    instrument_token: 13321218,
     name: "NESTLEIND",
   },
   {
-    instrument_token: "12553474",
+    instrument_token: 13278466,
+    name: "NIFTY",
+  },
+  {
+    instrument_token: 13321474,
     name: "NMDC",
   },
   {
-    instrument_token: "12553730",
+    instrument_token: 13321730,
     name: "NTPC",
   },
   {
-    instrument_token: "13758210",
+    instrument_token: 13322498,
+    name: "OBEROIRLTY",
+  },
+  {
+    instrument_token: 13322754,
     name: "OFSS",
   },
   {
-    instrument_token: "12553986",
+    instrument_token: 13323010,
     name: "ONGC",
   },
   {
-    instrument_token: "12554242",
+    instrument_token: 13323266,
     name: "PAGEIND",
   },
   {
-    instrument_token: "12554498",
+    instrument_token: 13323522,
     name: "PEL",
   },
   {
-    instrument_token: "12554754",
+    instrument_token: 13323778,
+    name: "PERSISTENT",
+  },
+  {
+    instrument_token: 13324034,
     name: "PETRONET",
   },
   {
-    instrument_token: "12555010",
+    instrument_token: 13324290,
     name: "PFC",
   },
   {
-    instrument_token: "12555266",
+    instrument_token: 13324546,
     name: "PFIZER",
   },
   {
-    instrument_token: "12555522",
+    instrument_token: 13324802,
     name: "PIDILITIND",
   },
   {
-    instrument_token: "12555778",
+    instrument_token: 13326082,
     name: "PIIND",
   },
   {
-    instrument_token: "12556034",
+    instrument_token: 13326338,
     name: "PNB",
   },
   {
-    instrument_token: "13761538",
+    instrument_token: 13326594,
     name: "POLYCAB",
   },
   {
-    instrument_token: "12556290",
+    instrument_token: 13326850,
     name: "POWERGRID",
   },
   {
-    instrument_token: "12556546",
+    instrument_token: 13327106,
     name: "PVR",
   },
   {
-    instrument_token: "12556802",
+    instrument_token: 14877954,
+    name: "RAIN",
+  },
+  {
+    instrument_token: 13327362,
     name: "RAMCOCEM",
   },
   {
-    instrument_token: "12557058",
+    instrument_token: 13327618,
     name: "RBLBANK",
   },
   {
-    instrument_token: "12557314",
+    instrument_token: 13327874,
     name: "RECLTD",
   },
   {
-    instrument_token: "12557570",
+    instrument_token: 13328642,
     name: "RELIANCE",
   },
   {
-    instrument_token: "12557826",
+    instrument_token: 13328898,
     name: "SAIL",
   },
   {
-    instrument_token: "12558082",
+    instrument_token: 13329154,
+    name: "SBICARD",
+  },
+  {
+    instrument_token: 13329410,
     name: "SBILIFE",
   },
   {
-    instrument_token: "12558338",
+    instrument_token: 13329666,
     name: "SBIN",
   },
   {
-    instrument_token: "12558594",
+    instrument_token: 13329922,
     name: "SHREECEM",
   },
   {
-    instrument_token: "12558850",
+    instrument_token: 13330178,
     name: "SIEMENS",
   },
   {
-    instrument_token: "12559106",
+    instrument_token: 13330434,
     name: "SRF",
   },
   {
-    instrument_token: "12559362",
+    instrument_token: 13330690,
     name: "SRTRANSFIN",
   },
   {
-    instrument_token: "13288194",
+    instrument_token: 13330946,
     name: "STAR",
   },
   {
-    instrument_token: "12559618",
+    instrument_token: 13331202,
     name: "SUNPHARMA",
   },
   {
-    instrument_token: "12559874",
+    instrument_token: 13331458,
     name: "SUNTV",
   },
   {
-    instrument_token: "13766658",
+    instrument_token: 13331714,
     name: "SYNGENE",
   },
   {
-    instrument_token: "12560130",
+    instrument_token: 13331970,
     name: "TATACHEM",
   },
   {
-    instrument_token: "12560386",
+    instrument_token: 14883074,
+    name: "TATACOMM",
+  },
+  {
+    instrument_token: 13332226,
     name: "TATACONSUM",
   },
   {
-    instrument_token: "12560642",
+    instrument_token: 13332482,
     name: "TATAMOTORS",
   },
   {
-    instrument_token: "12560898",
+    instrument_token: 13332738,
     name: "TATAPOWER",
   },
   {
-    instrument_token: "12561154",
+    instrument_token: 13332994,
     name: "TATASTEEL",
   },
   {
-    instrument_token: "12568834",
+    instrument_token: 13334274,
     name: "TCS",
   },
   {
-    instrument_token: "12569090",
+    instrument_token: 13334530,
     name: "TECHM",
   },
   {
-    instrument_token: "12569346",
+    instrument_token: 13334786,
     name: "TITAN",
   },
   {
-    instrument_token: "12569602",
+    instrument_token: 13335042,
     name: "TORNTPHARM",
   },
   {
-    instrument_token: "12569858",
+    instrument_token: 13335298,
     name: "TORNTPOWER",
   },
   {
-    instrument_token: "12570114",
+    instrument_token: 13335554,
     name: "TRENT",
   },
   {
-    instrument_token: "12570370",
+    instrument_token: 13335810,
     name: "TVSMOTOR",
   },
   {
-    instrument_token: "12570626",
+    instrument_token: 13336066,
     name: "UBL",
   },
   {
-    instrument_token: "12570882",
+    instrument_token: 13336834,
     name: "ULTRACEMCO",
   },
   {
-    instrument_token: "12571138",
+    instrument_token: 13337090,
     name: "UPL",
   },
   {
-    instrument_token: "12572418",
+    instrument_token: 13337858,
     name: "VEDL",
   },
   {
-    instrument_token: "12572674",
+    instrument_token: 13338114,
     name: "VOLTAS",
   },
   {
-    instrument_token: "12572930",
+    instrument_token: 13338370,
+    name: "WHIRLPOOL",
+  },
+  {
+    instrument_token: 13338626,
     name: "WIPRO",
   },
   {
-    instrument_token: "12573186",
+    instrument_token: 13339906,
     name: "ZEEL",
   },
 ];
-
 raw.forEach(({ instrument_token, name }) => {
-  SCRIPTS[name] = parseInt(instrument_token);
+  SCRIPTS[name] = instrument_token;
 });
